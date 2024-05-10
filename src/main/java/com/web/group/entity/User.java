1923c8fc4a2bdeb7ca0ba317d203bcb5e019a8e7
chashao <chashao246972@gmail.com>
2024-05-10 11:52:04 +0800

    添加数据库类

---
+++ src/main/java/com/web/group/entity/User.java
@@ -0,0 +1,20 @@
+package com.web.group.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class User implements Serializable {
+    private static final long serialVersionUID = 6570002271842385911L;
+
+    private Integer userId;
+
+    private String userName;
+
+    private String userPassword;
+
+    private Date createTime;
+
+}
